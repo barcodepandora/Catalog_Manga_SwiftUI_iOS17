8829be37cb58f5dfdb2d383d18eb9a47ca4f2395
Juan <jmoreno@novopayment.com>
2024-08-18 15:11:12 -0500

    Stub for watch

---
+++ ParrotDayDraftRequestManga/MyMangaForWatch Watch App/MyMangaForWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMangaForWatchApp.swift
+//  MyMangaForWatch Watch App
+//
+//  Created by Juan Manuel Moreno on 18/08/24.
+//
+
+import SwiftUI
+
+@main
+struct MyMangaForWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
